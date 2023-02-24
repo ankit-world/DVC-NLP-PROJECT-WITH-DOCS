7008b377df9bd7c140e0f84afe96deb86eaf49a9
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-02-24 22:01:40 +0530

    first commit

--- init_setup.sh
+++ init_setup.sh
@@ -1,12 +1,18 @@
 echo [$(date)]: "START"
-export _VERSION_=3.8
-echo [$(date)]: "creating environment with python ${_VERSION_}"
-conda create --prefix ./env python=${_VERSION_} -y
+echo [$(date)]: "creating environment"
+conda create --prefix ./env python=3.7 -y
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
+echo [$(date)]: "export conda environment"
+conda env export > conda.yaml
+echo "# ${PWD}" > README.md
+echo [$(date)]: "first commit"
+git add .
+git commit -m "first commit"
 echo [$(date)]: "END"
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
+
