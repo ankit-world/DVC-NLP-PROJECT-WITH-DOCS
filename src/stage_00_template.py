0a2edc3a8ec1c5d3f8cc0c7a8ea42e83f1856d2a
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-02-25 15:02:15 +0530

    template stage updated

--- src/stage_00_template.py
+++ src/stage_00_template.py
@@ -1,27 +1,39 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
 import logging
-from src.utils import save_json
-import time
-from src import logger
-from src.constants import *
-from src.components import StageClass
+from src.utils.common import read_yaml, create_directories
+import random
 
 
-STAGE = "Stage name" ## <<< change stage name 
+STAGE = "STAGE_NAME" ## <<< change stage name 
 
-# init logger
-logger()
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
 
 
-
-def main():
-    obj = StageClass()
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+    pass
 
 
 if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main()
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
