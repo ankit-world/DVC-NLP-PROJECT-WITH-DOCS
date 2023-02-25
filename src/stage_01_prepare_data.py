623249d1556fa62c359ec078bc8f2e6c5c9deb16
Ankit Marwaha <ankit.marwaha@nagarro.com>
2023-02-25 15:46:07 +0530

    data preparation done

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import logging
 from src.utils import read_yaml, create_directories
+from src.utils.data_mgmt import process_posts
 import random
 
 
@@ -26,7 +27,7 @@ def main(config_path, params_path):
 
     split = params["prepare"]["split"] # split ratio
     seed = params["prepare"]["seed"]
-    #tag = params["prepare"]["tag"]
+    tag = params["prepare"]["tag"]
 
     random.seed(seed)
 
@@ -34,14 +35,14 @@ def main(config_path, params_path):
     prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
     create_directories([prepare_data_dir_path])
 
-    #train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
-    #test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
+    train_data_path = os.path.join(prepare_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepare_data_dir_path,artifacts["TEST_DATA"])
 
-    # encode = "utf8"
-    # with open(source_data_path, encoding=encode) as fd_in: # actual input data that we are reading
-    #     with open(train_data_path, "w", encoding=encode) as fd_out_train: # writing train data
-    #         with open(test_data_path, "w", encoding=encode) as fd_out_test: # writing test data
-    #             process_posts(fd_in, fd_out_train, fd_out_test, tag, split)
+    encode = "utf8"
+    with open(source_data_path, encoding=encode) as fd_in: # actual input data that we are reading
+        with open(train_data_path, "w", encoding=encode) as fd_out_train: # writing train data
+            with open(test_data_path, "w", encoding=encode) as fd_out_test: # writing test data
+                process_posts(fd_in, fd_out_train, fd_out_test, tag, split)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
